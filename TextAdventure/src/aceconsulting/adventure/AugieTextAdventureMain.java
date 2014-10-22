952315a5457d7aa6d2a231075e617fb7077941d5
fstonedahl <forreststonedahl@augustana.edu>
2014-10-22 10:39:08 -0500

    Created a world & player object in main()...

--- TextAdventure/src/aceconsulting/adventure/AugieTextAdventureMain.java
+++ TextAdventure/src/aceconsulting/adventure/AugieTextAdventureMain.java
@@ -3,6 +3,8 @@ package aceconsulting.adventure;
 public class AugieTextAdventureMain {
 
 	public static void main(String[] args) {
+		World theWorld = new AugieDuckWorld();
+		Player thePlayer = new Player(theWorld.getStartLocation());
 		
 
 	}
